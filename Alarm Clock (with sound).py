0346d5e51cbc8ac25f0c9caf0468fdf5552edcde
Official-Aryan-chauhan <nothing30103@gmail.com>
2026-01-14 10:22:43 +0530

    File code

---
+++ Alarm Clock (with sound).py
@@ -0,0 +1,56 @@
+# Alarm clcok ! For Singel Alarm System with digitl clock
+import time 
+from playsound3 import playsound
+import os
+import sys
+import keyboard
+# Feature to add -- add multi alarm system
+time_ = time.strftime("%I:%M:%S %p")
+print(f"Current Time | {time_}")
+# time.sleep(2)
+
+temp_list  = []
+alarm_H = (input("Enter Time of Alarm Hour : ")).strip()
+alarm_M = (input("Enter Time of Alarm Minute : ")).strip()
+alarm_AM_PM = input("Enter AM ro PM : ").upper().strip()
+temp_list.append(alarm_H)
+temp_list.append(alarm_M)
+temp_list.append(alarm_AM_PM)
+
+def clear_terminal():
+    if os.name == "nt":
+        _ = os.system('cls')
+    else:
+        _  = os.system("clear")  
+        # ("Cleared Terminal")
+
+def ring_alarm():
+        print(time_)
+        print("Ringing !")
+        try:
+            playsound("E:\Git and Github\Alarm-clock-Python-project\sound.mp3")
+        except Exception as e:
+            print("Error : ", e)
+            sys.exit(0)
+
+while True:
+
+    if  keyboard.is_pressed('q'):
+        print("Alarm Clock Stopped !")
+        break
+    else:
+        if temp_list[0] == time.strftime('%I') and temp_list[1] == time.strftime('%M') and temp_list[2] == time.strftime("%p"):
+            ring_alarm()
+        
+        else:
+            print(time.strftime("%I:%M:%S %p"))
+            time.sleep(1) 
+            clear_terminal()
+            continue
+   
+        
+
+# End of the code
+
+        
+        
